c22fba4649932d631122057b5ba17204236f4ab7
Ryomasao <konoemario@gmail.com>
2023-05-01 06:00:20 +0000

    01

--- sources/forth_modoki/01_int_parser/int_parser.c
+++ sources/forth_modoki/01_int_parser/int_parser.c
@@ -1,21 +1,49 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-static const char* const input = "123 456  1203";
+static const char *const input = "123 456  1203";
+int position = 0;
 
+int main()
+{
+    // write something here.
+    int unit = 0;
 
-int main() {
+    int a[3] = {};
+    int index = 0;
+    int row = 0;
+
+    while (input[position])
+    {
+        if (isdigit(input[position]))
+        {
+            a[row] = a[row] * 10;
+            a[row] += input[position] - '0';
+        }
+        else
+        {
+            if (a[row] != 0)
+            {
+                row++;
+            }
+        }
+        position++;
+    }
+
     int answer1 = 0;
     int answer2 = 0;
     int answer3 = 0;
 
-    // write something here.
-
+    answer1 = a[0];
+    answer2 = a[1];
+    answer3 = a[2];
 
     // verity result.
     assert(answer1 == 123);
     assert(answer2 == 456);
     assert(answer3 == 1203);
-
-    return 0;
 }
